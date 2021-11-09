c8e7478675ece28d6d0ab1ed3c4f614dcee3a886
Ogawato <t-ogawa@yairilab.net>
2021-11-09 19:20:54 +0900

    詳細ページコンテンツ表示

--- src/templates/blogDetail.js
+++ src/templates/blogDetail.js
@@ -1,8 +1,32 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import { Container } from "react-bootstrap"
 
-const BlogDetail = () => {
-  return <Layout>konnnitihahahah</Layout>
+export const query = graphql`
+  query ($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        data
+      }
+    }
+  }
+`
+
+const BlogDetail = props => {
+  return (
+    <Layout>
+      <Container style={{ maxWidth: 640 }} className="pt-4">
+        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+        <p>{props.data.markdownRemark.frontmatter.data}</p>
+        <div
+          dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+        ></div>
+      </Container>
+    </Layout>
+  )
 }
 
 export default BlogDetail
