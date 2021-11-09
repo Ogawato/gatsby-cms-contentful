80f65edb25c750a371deff8ec82a5769fddc014b
Ogawato <t-ogawa@yairilab.net>
2021-11-09 18:48:27 +0900

    詳細ページの作成

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,7 @@
 //     defer: true,
 //   })
 // }
+const { create } = require("domain")
 const path = require("path")
 
 exports.onCreateNode = ({ node, actions }) => {
@@ -23,4 +24,31 @@ exports.onCreateNode = ({ node, actions }) => {
   }
   // Transform the new node here and create a new node or
   // create a new node field.
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const blogPostTemplate = path.resolve(`src/templates/blogDetail.js`)
+
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    createPage({
+      path: `/blog/${edge.node.fields.slug}`,
+      component: blogPostTemplate,
+      context: { slug: edge.node.fields.slug },
+    })
+  })
 }
---
+++ src/templates/blogDetail.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const BlogDetail = () => {
+  return <Layout>konnnitihahahah</Layout>
+}
+
+export default BlogDetail
